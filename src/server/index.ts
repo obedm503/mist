5a4d72ed382fc7ff5739e31a1ebcde978a6c75a3
obedm503 <obedm503@gmail.com>
2017-10-28 13:10:16 -0600

    add build/

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
 // Save your local vars in .env for testing. DO NOT VERSION CONTROL `.env`!.
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') require('dotenv').config();
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') require('dotenv').config();
 
 import * as Koa from 'koa';
 import * as bodyParser from 'koa-bodyparser';
@@ -7,14 +7,23 @@ import * as chalk from 'chalk';
 import * as koaStatic from 'koa-static';
 import * as Router from 'koa-router';
 import { join } from 'path';
-
+import webpack from 'webpack';
+import webpackDev from 'koa-webpack-dev-middleware';
+import config from '../../config';
 import router from './routes';
 
 const app = new Koa();
 const port = process.env.PORT || 5555;
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(koaStatic(join(__dirname, '../client/')) as any);
+} else {
+  app.use(webpackDev(webpack(config), {
+    publicPath: '/',
+    quiet: true
+  }));
+}
 app.use(bodyParser())
-  .use(koaStatic(join(__dirname, '../client/')) as any)
   .use(router.routes())
   .use(router.allowedMethods());
 
